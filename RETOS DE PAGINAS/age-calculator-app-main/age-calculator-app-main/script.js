a6fa9a1be18816fe5f7f7673c4d3af5ad6aeefde
waltermp11 <walterm200011@gmail.com>
2023-06-07 21:20:14 -0500

    PrimerCommit

---
+++ RETOS DE PAGINAS/age-calculator-app-main/age-calculator-app-main/script.js
@@ -0,0 +1,17 @@
+
+// Capturamos los datos
+
+const day = document.querySelector("input[name='day']");
+const  month= document.querySelector("input[name='month']");
+const year  = document.querySelector("input[name='year']");
+
+console.log(anioDeNacimiento);
+
+
+
+//funcion para calcular edad de la persona 
+function calcularEdad(fecha) {
+    
+    const age= new Date().get();
+    
+}
